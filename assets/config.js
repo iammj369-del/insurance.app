bb3e0e6a93b423b7ef0c4cf589ea7e5896dd6bae
Nandhakumar369 <indradevinss337@gmail.com>
2026-06-09 15:31:18 +0530

    Updated supabase configuration

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,5 @@
-export const SUPABASE_URL = "https://aojqqijrxlbblpcftevm.supabase.co";
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFvanFxaWpyeGxiYmxwY2Z0ZXZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA4OTAyODQsImV4cCI6MjA5NjQ2NjI4NH0.4CKsAWXOYAi7CrP0F8BNS1XEyB4u3tuP7gLUElydNKs";
-
+export const SUPABASE_URL = "https://bbcpdrfhjvidqvuiwlft.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJiY3BkcmZoanZpZHF2dWl3bGZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5OTIxMTEsImV4cCI6MjA5NjU2ODExMX0.yQXBdYPW8HCgjYX6Ho4WWAn6he7d0unSe-SraCvyEQM";
 export const APP_CONFIGURED =
   SUPABASE_URL.startsWith("https://") &&
   SUPABASE_ANON_KEY.length > 40;
